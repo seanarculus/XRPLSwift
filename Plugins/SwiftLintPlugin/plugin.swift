914394ccd9b8c619767e5e45da8bd4f5ece5bc84
Sean Orelli <sorelli@arculus.co>
2025-06-04 12:56:21 -0400

    removes lint command

--- Plugins/SwiftLintPlugin/plugin.swift
+++ Plugins/SwiftLintPlugin/plugin.swift
@@ -5,19 +5,6 @@ struct SwiftLintPlugins: BuildToolPlugin {
 
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         [
-            .buildCommand(
-                displayName: "Linting \(target.name)...",
-                executable: try context.tool(named: "swiftlint").path,
-                arguments: [
-                    "lint",
-                    "--in-process-sourcekit",
-                    "--config",
-                    "\(context.package.directory.string)/Plugins/SwiftLintPlugin/swiftlint.yml",
-                    "--path",
-                    target.directory.string   // only lint the files in the target directory
-                ],
-                environment: [:]
-            )
         ]
     }
 }
